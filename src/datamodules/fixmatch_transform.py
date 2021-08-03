db066560c45426cced5d791628feeaadd337aeb3
scarecrow1123 <ananda.seelan@gmail.com>
2021-08-03 22:00:03 +0530

    Drop RandomCrop and resize to 224, 224

--- src/datamodules/fixmatch_transform.py
+++ src/datamodules/fixmatch_transform.py
@@ -14,7 +14,7 @@ class FixMatchImageTransform(object):
     def __init__(self, pre_trfms, post_trfms):
         self.weak = transforms.Compose(
             [
-                transforms.Resize(size=(300, 300)),
+                transforms.Resize(size=(224, 224)),
                 transforms.RandomHorizontalFlip(),
                 # transforms.RandomCrop(
                 #     size=32, padding=int(32 * 0.125), padding_mode="reflect"
@@ -23,7 +23,7 @@ class FixMatchImageTransform(object):
         )
         self.strong = transforms.Compose(
             [
-                transforms.Resize(size=(300, 300)),
+                transforms.Resize(size=(224, 224)),
                 transforms.RandomHorizontalFlip(),
                 # transforms.RandomCrop(
                 #     size=32, padding=int(32 * 0.125), padding_mode="reflect"
